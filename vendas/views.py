8d56e1c537516d772f253e8bb7eed1500f4f638c
barbozae <155493220+barbozae@users.noreply.github.com>
2025-01-02 22:09:06 -0300

    melhoria permissões Vendas

--- vendas/views.py
+++ vendas/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin # validação da permissão do usuário
-
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Vendas
 
 
 def vendas_resumo(request):
     return render(request, 'vendas/vendas_resume.html')
 
-# Função para verificar se o usuário é membro da equipe
-def is_team_member(user):
-    # Verifica se o usuário é um superusuário ou se é membro do grupo 'Equipe'
-    return user.is_superuser or user.groups.filter(name='Vendas').exists()
-
-class VendasListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
-# class VendasListView(ListView):
+class VendasListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Vendas
     template_name = 'vendas_list.html'  # Garante que o template correto será usado
+    permission_required = 'vendas.view_vendas'  # Permissão para visualizar objetos'
 
     def get_queryset(self):
         queryset = Vendas.objects.all()
@@ -59,19 +52,14 @@ class VendasListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
         context['vendas_total'] = total_geral
         return context
 
-    # Avaliar se usuário tem acesso a view retornando True ou False
-    def test_func(self):
-        return is_team_member(self.request.user)
-
-    # Caso a função test_func retorna False essa função redireciona para home
+    # caso usuário não tenha acesso conforme permissões consedidas seria direcionado para página Home
     def handle_no_permission(self):
         if not self.request.user.is_authenticated:
-            return self.handle_no_permission()  # Redireciona para a página de login
-        return redirect('home')  # Redireciona para home se não for membro da equipe
+            return redirect('login')  # Redireciona para a página de login
+        return redirect('home')  # Redireciona para a home se o usuário não tiver permissão
 
 
-class VendasCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
-# class VendasCreateView(CreateView):
+class VendasCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Vendas
     # campos que o usuario precisa preencher
     fields = ["data_venda", "periodo", "rodizio", "dinheiro", "pix", "debito_mastercard", "debito_visa", 
@@ -79,56 +67,47 @@ class VendasCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
               "hiper", "sodexo", "ticket_rest", "vale_refeicao", "dinersclub", "socio"]
     # após o salvamentos redireciona para a view vendas
     success_url = reverse_lazy("venda_list")
+    permission_required = 'vendas.add_vendas'
 
     def form_valid(self, form):
         # Adiciona o usuário logado como autor
         form.instance.author = self.request.user
         return super().form_valid(form)
     
-    # Avaliar se usuário tem acesso a view retornando True ou False
-    def test_func(self):
-        return is_team_member(self.request.user)
-
-    # Caso a função test_func retorna False essa função redireciona para home
+    # caso usuário não tenha acesso conforme permissões consedidas seria direcionado para página Home
     def handle_no_permission(self):
         if not self.request.user.is_authenticated:
-            return self.handle_no_permission()
-        return redirect('home')
+            return redirect('login')  # Redireciona para a página de login
+        return redirect('home')  # Redireciona para a home se o usuário não tiver permissão
 
 
-class VendasUpDateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-# class VendasUpDateView(UpdateView):
+class VendasUpDateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Vendas
     fields = ["data_venda", "periodo", "rodizio", "dinheiro", "pix", "debito_mastercard", "debito_visa", 
               "debito_elo", "credito_mastercard", "credito_visa", "credito_elo", "alelo", "american_express",
               "hiper", "sodexo", "ticket_rest", "vale_refeicao", "dinersclub", "socio"]
     success_url = reverse_lazy("venda_list")
+    permission_required = 'vendas.change_vendas'
 
     def form_valid(self, form):
         # Adiciona o usuário logado como autor
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
-    # Avaliar se usuário tem acesso a view retornando True ou False
-    def test_func(self):
-        return is_team_member(self.request.user)
 
-    # Caso a função test_func retorna False essa função redireciona para home
+    # caso usuário não tenha acesso conforme permissões consedidas seria direcionado para página Home
     def handle_no_permission(self):
         if not self.request.user.is_authenticated:
-            return self.handle_no_permission()
-        return redirect('home')
+            return redirect('login')  # Redireciona para a página de login
+        return redirect('home')  # Redireciona para a home se o usuário não tiver permissão
 
 
-class VendasDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-# class VendasDeleteView(DeleteView):
+class VendasDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Vendas
     success_url = reverse_lazy("venda_list")
+    permission_required = 'vendas.delete_vendas'
 
-    def test_func(self):
-        return is_team_member(self.request.user)
-
+    # caso usuário não tenha acesso conforme permissões consedidas seria direcionado para página Home
     def handle_no_permission(self):
         if not self.request.user.is_authenticated:
-            return self.handle_no_permission()
-        return redirect('home')
+            return redirect('login')  # Redireciona para a página de login
+        return redirect('home')  # Redireciona para a home se o usuário não tiver permissão
